d884aad3987e376442dd13e8a78fa1beaa724af5
dladncks1217 <dlaxodud1217@gmail.com>
2020-06-03 19:23:32 +0900

    #1 first init

--- back/index.js
+++ back/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
