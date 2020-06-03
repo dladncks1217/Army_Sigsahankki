9b0ea8d3aab4506a093c7a93843a0eaba5598937
dladncks1217 <dlaxodud1217@gmail.com>
2020-06-03 18:46:48 +0900

    first init

---
+++ back/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+
+require('dotenv').config();
+
+const app = express();
+
+
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({   
+    resave:false,
+    saveUninitialized:false,
+    secret:process.env.COOKIE_SECRET,
+    cookie:{
+        httpOnly:true,
+        secure:false,
+    },
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req,res,next)=>{
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err,req,res)=>{
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err:{};
+    res.status(err.status||500);
+    res.render('error');
+});
+
+app.listen(app.get(process.env.PORT),()=>{
+    console.log(`${process.env.PORT} 번 포트에서 서버 대기중`);
+});
